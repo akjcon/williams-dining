ad7e7393f202c5fa5c18d849d4d8debbc7133fe6
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-02 14:16:36 -0400

    Implemented a simple fade transition for loading.

--- williams-dining/CentralNavigationController.swift
+++ williams-dining/CentralNavigationController.swift
@@ -9,7 +9,12 @@
 import Foundation
 import UIKit
 
-class CentralNavigationController: UINavigationController {
+class CentralNavigationController: UINavigationController, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+    }
     
     let loadingViewController: LoadingViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LoadingViewController") as! LoadingViewController
     let mainViewController: CentralTabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MainViewController") as! CentralTabBarController
@@ -28,9 +33,52 @@ class CentralNavigationController: UINavigationController {
         loadingViewController.stopTimer()
         self.popViewControllerAnimated(true)
 
-//        mainViewController.displayLoadingError()
-        mainViewController.selectedIndex = 2
-        
+        mainViewController.displayLoadingError()
     }
 
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+
+        if operation == UINavigationControllerOperation.Push {
+            return PushAnimator()
+        }
+
+        if operation == UINavigationControllerOperation.Pop {
+            return PopAnimator()
+        }
+        return nil
+    }
+
+}
+
+class PushAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.5
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        transitionContext.containerView()?.addSubview(toViewController.view)
+        UIView.animateWithDuration(self.transitionDuration(transitionContext), animations: {
+            toViewController.view.alpha = 1.0
+        }) {(finished: Bool) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+        }
+    }
+}
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.5
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        transitionContext.containerView()?.insertSubview(toViewController.view, belowSubview: fromViewController.view)
+        UIView.animateWithDuration(self.transitionDuration(transitionContext), animations: {
+            fromViewController.view.alpha = 0.0
+        }) {(finished: Bool) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+        }
+    }
 }
