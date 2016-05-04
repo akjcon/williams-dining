d7028f363ae314d842f0d546a167d244e1972212
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-04 15:14:04 -0400

    Android app.

---
+++ EphMeals/app/src/main/java/com/nsandersen/ephmeals/DiningHallActivity.java
@@ -0,0 +1,13 @@
+package com.nsandersen.ephmeals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiningHallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_hall);
+    }
+}
