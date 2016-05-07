b80d4a0ffdc6beb9e010d29d4fa6f70c033187d4
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-06 20:46:25 -0400

    started the project over again.

--- EphMeals/app/src/main/java/com/nsandersen/ephmeals/MainActivity.java
+++ EphMeals/app/src/main/java/com/nsandersen/ephmeals/MainActivity.java
@@ -3,11 +3,11 @@ package com.nsandersen.ephmeals;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DiningHallActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dining_hall);
+        setContentView(R.layout.activity_main);
     }
 }
