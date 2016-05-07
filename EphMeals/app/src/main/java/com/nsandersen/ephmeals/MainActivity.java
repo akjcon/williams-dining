16244b4a36fd61983b45f1dfd431af5694490292
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-07 12:31:46 -0400

    android development.

--- EphMeals/app/src/main/java/com/nsandersen/ephmeals/MainActivity.java
+++ EphMeals/app/src/main/java/com/nsandersen/ephmeals/MainActivity.java
@@ -5,9 +5,25 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.recipe_list_view);
+// 1
+        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
+// 2
+        String[] listItems = new String[recipeList.size()];
+// 3
+        for(int i = 0; i < recipeList.size(); i++){
+            Recipe recipe = recipeList.get(i);
+            listItems[i] = recipe.title;
+        }
+// 4
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
     }
 }
